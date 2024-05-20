b15ed705a4d6e84fb6bc803a150c4b50b75ad660
EdgarLemusSQA <edgar.lemus@sqasa.co>
2024-05-20 16:38:29 -0500

    Create Web Project Node js

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DATABASE_HOST || "localhost",
-  user: process.env.DATABASE_USER || "fazt",
-  password: process.env.DATABASE_PASSWORD || "mypassword",
-  database: process.env.DATABASE_NAME || "linksdb",
-  port: process.env.DATABASE_PORT || 3306,
+  host: process.env.DATABASE_HOST || "roundhouse.proxy.rlwy.net",
+  user: process.env.DATABASE_USER || "root",
+  password: process.env.DATABASE_PASSWORD || "JXbdvsKgAFGOMHeURpbVENHXGBWnOdGL",
+  database: process.env.DATABASE_NAME || "railway",
+  port: process.env.DATABASE_PORT || 30251,
 };
 
 export const port = process.env.PORT || 4000;
